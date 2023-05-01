c88db46d288e2794a72ec0bc04558072fa2f0dd6
PavloYusyk <pavloyusyk@gmail.com>
2023-05-01 21:34:27 +0300

    new js

---
+++ Yusyk_hw1/main.js
@@ -0,0 +1,55 @@
+// завдання 1
+let s1 = 'hello';
+console.log(s1);
+let s2 = 'owu';
+console.log(s2);
+let s3 = 'com';
+console.log(s3);
+
+let num1 = 1;
+console.log(num1);
+let num2 = 100;
+console.log(num2);
+let num3 = -999;
+console.log(num3);
+let num4 = 123;
+console.log(num4);
+let num5 = 3.14;
+console.log(num5);
+let num6 = 2.7;
+console.log(num6);
+let num7 = 16;
+console.log(num7);
+
+let bool = true;
+console.log(bool);
+let bool1 = false;
+console.log(bool1);
+
+// завдання 2
+let firstName = 'Юсик';
+let middleName = 'Павло';
+let lastName = 'Миронович';
+
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+
+// завдання 3
+let a = 100;
+console.log(typeof a);
+console.log(a);
+let b = '100';
+console.log(typeof b);
+console.log(b);
+let c = true;
+console.log(typeof c);
+console.log(c);
+
+// дод.завдання
+
+let myName = prompt('Enter your name:');
+let surName = prompt('Enter your surname:');
+let yearOld = prompt('How old are you?')
+
+let user = `My name is ${myName}, surname ${surName}, I have ${yearOld} year old!`;
+console.log(user);
